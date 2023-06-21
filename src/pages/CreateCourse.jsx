76c8a0ea3f53275db7803305afaf714b8b309f78
khinezinthin <khinezinthinwk1@gmail.com>
2023-06-21 11:21:25 +0630

    create-gap

--- src/pages/CreateCourse.jsx
+++ src/pages/CreateCourse.jsx
@@ -10,13 +10,13 @@ const CreateCourse = () => {
   return (
     <div>
       <div className="flex justify-center ">
-        <div className=" w-[95%] max-[900px]:w-[95% max-[900px]:w-[94%] max-[600px]:w-[92%] mt-10 mb-6  bgTransparent">
-          <form className=" mx-10 mb-16">
+        <div className=" w-[95%] max-[900px]:w-[95% max-[900px]:w-[94%] max-[600px]:w-[92%] mt-10 mb-6 bgTransparent">
+          <form className=" mx-10 max-sm:mx-5 mb-16">
             <h1 className=" text-4xl text-white text-shadow my-10 max-[435px]:text-3xl max-[380px]:text-2xl createFamily tracking-wide">
               Create New Course
             </h1>
             <div className=" flex flex-col">
-              <div className=" flex max-[900px]:flex-col gap-9 items-center max-[900px]:items-start">
+              <div className=" flex max-[900px]:flex-col max-[900px]:gap-5 gap-9 items-center max-[900px]:items-start">
                 <div className=" w-[30%] max-[900px]:w-[50%] max-[700px]:w-[70%] max-[600px]:w-[80%] max-[500px]:w-[100%]">
                   <div
                     onClick={() => {
@@ -36,12 +36,12 @@ const CreateCourse = () => {
                 </div>
 
                 <div className=" w-[70%] max-[900px]:w-full">
-                    <div className='flex flex-col gap-8'>
-                        <div className=' flex flex-col gap-7'>
+                    <div className='flex flex-col gap-8 max-[900px]:gap-5'>
+                        <div className=' flex flex-col gap-7 max-[900px]:gap-5'>
                           <input className='   py-3 pl-4 custom-input rounded-md' type='text' placeholder="Title"/>
                           <input className='  custom-input pt-4 pb-20  pl-4 rounded-md' type='text' placeholder="Description"/>
 
-                          <div className=' flex gap-5 w-[100%] max-[700px]:flex-col max-[700px]:gap-8'>
+                          <div className=' flex gap-5 w-[100%] max-[700px]:flex-col'>
                             <input className='  custom-input w-full py-3 pl-4 rounded-md' type='number' placeholder="Duration" />
                             <input className='  custom-input w-full py-3 pl-4 rounded-md' type='number' placeholder="Lessons" />
                             <input className='  custom-input w-full py-3 pl-4 rounded-md' type='text' placeholder="Language" />
@@ -55,7 +55,7 @@ const CreateCourse = () => {
 
               <div
                 onClick={() => inputVideo.current.click()}
-                className=" custom-input flex justify-between cursor-pointer w-full mb-8 mt-8 py-4 pl-4 mr-1  border-[#e5eaef] rounded-md">
+                className=" custom-input flex justify-between cursor-pointer w-full mb-8 max-[900px]:mb-5 mt-8 max-[900px]:mt-5 py-4 pl-4 mr-1  border-[#e5eaef] rounded-md">
                 <p className="subtitle">Upload video</p>
                 <input
                   className=" w-[900px] border"
