de099b8320cd5d82d2ad2b24818dd46054a939de
khinezinthin <khinezinthinwk1@gmail.com>
2023-06-29 18:31:17 +0630

    change-show

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import themeSlice from "./themeSlice";
 import authSlice from "./authSlice";
 import { authApi } from "./api/authApi";
-import showComponentsSlice from "./showComponentsSlice";
 
 export const store = configureStore({
   reducer: {
     themeSlice: themeSlice,
     [authApi.reducerPath]: authApi.reducer,
     authSlice: authSlice,
-    showComponentsSlice: showComponentsSlice
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(authApi.middleware),
