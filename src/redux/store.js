39ffbf779e3c0132b962425c14c76ee82bbea183
khinezinthin <khinezinthinwk1@gmail.com>
2023-06-29 15:59:07 +0630

    test-show

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import themeSlice from "./themeSlice";
 import authSlice from "./authSlice";
 import { authApi } from "./api/authApi";
+import showComponentsSlice from "./showComponentsSlice";
 
 export const store = configureStore({
   reducer: {
     themeSlice: themeSlice,
     [authApi.reducerPath]: authApi.reducer,
     authSlice: authSlice,
+    showComponentsSlice: showComponentsSlice
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(authApi.middleware),
--- src/route/Path.jsx
+++ src/route/Path.jsx
@@ -1,53 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "../pages/Dashboard";
+import {Route, Routes } from "react-router-dom";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
 import ResetPassword from "../pages/ResetPassword";
-import CreateCourse from "../pages/CreateCourse";
-import Theme from "../components/Theme";
-import Profile from "../components/Profile/Profile";
-import Navbar from "../components/Navbar";
-import Error from "../pages/Error";
-
-import Calender from "../components/Calender/Calender";
-
-import { StateContextCustom } from "../components/context/StateContext";
-import StudentTable from "../components/StudentTable";
-import CourseTable from "../components/CourseTable";
-import TeacherTable from "../components/TeacherTable";
 import ScrollTop from "../components/ScrollTop";
+import Home from "../pages/Home";
 
 const Path = () => {
-  const { isSidebarOpen } = StateContextCustom();
   return (
     <div>
-      {/* <BrowserRouter> */}
       <ScrollTop/>
-        {/* <div className="flex flex-col">
-          <div>
-            <Navbar />
-          </div>
-
-          <div className={`mt-[52px] duration-500 ${isSidebarOpen && " ml-[230px]" } max-lg:ml-0`}> */}
-
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/resetPassword" element={<ResetPassword />} />
-              {/* <Route path="/createCourse" element={<CreateCourse />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="*" element={<Error />} />
-              <Route path="/calender" element={<Calender />} />
-              <Route path="/studentTable" element={<StudentTable />} />
-              <Route path="/courseTable" element={<CourseTable />} />
-              <Route path="/teacherTable" element={<TeacherTable />} /> */} 
-            </Routes>
-          {/* </div>
-        </div> */}
-        {/* <Theme /> */}
-      {/* </BrowserRouter> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/resetPassword" element={<ResetPassword />} />
+      </Routes>
     </div>
   );
 };
