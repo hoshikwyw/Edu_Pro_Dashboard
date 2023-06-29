b007ac62cdd01442585f829917b6b2ed36e41f73
khinezinthin <khinezinthinwk1@gmail.com>
2023-06-29 16:57:01 +0630

    showComponents-done

--- src/redux/showComponentsSlice.js
+++ src/redux/showComponentsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  showComponents: "Dashboard",
+  showComponents: 0,
 };
 
 export const showComponentsSlice = createSlice({
