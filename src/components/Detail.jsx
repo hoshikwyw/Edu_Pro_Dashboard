fa9c22533281451d1a6305c1d01dac372694cd1a
khinezinthin <khinezinthinwk1@gmail.com>
2023-06-30 21:46:36 +0630

    detail-responsive-done

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -21,102 +21,102 @@ const Detail = () => {
       </div>
       <div className={`mt-[52px] duration-500 ${isSidebarOpen && " ml-[230px]" } max-lg:ml-0`}>
         <div className=' flex justify-center'>
-          <div className=" bgTransparent w-[95%] my-10 rounded flex justify-between ">
+          <div className=" bgTransparent w-[95%] my-10 rounded flex justify-between max-xl:flex-col">
             {/* detail left start */}
-            <div className=' flex justify-center w-[45%] text-white'>
+            <div className='flex justify-center w-[45%] max-xl:w-full text-white'>
               <div className=' flex flex-col'>
                 {/* image div  */}
-                <div className=' relative p-10 '>
-                  <img className=' w-[500px] h-[240px] rounded' src="	https://eduvibe.devsvibe.com/main/wp-content/themes/eduvibe/assets/images/course-preview.png" alt="" />
-                  <p className=' absolute top-[40%] left-[45%] text-[50px]'><BsFillPlayCircleFill/></p>
+                <div className=' relative p-10 max-[500px]:p-5'>
+                  <img className=' w-[500px] h-[240px] max-xl:w-[900px] max-xl:h-[350px] max-md:h-[240px] rounded-lg' src="	https://eduvibe.devsvibe.com/main/wp-content/themes/eduvibe/assets/images/course-preview.png" alt="" />
+                  <p className=' absolute top-[40%] left-[45%] text-[50px] max-xl:text-[60px] max-md:text-[50px] text-blue-500'><BsFillPlayCircleFill/></p>
                 </div>
 
                 {/* detail  */}
-                <div className='px-10 pb-10'>
+                <div className='px-10 max-[500px]:px-5'>
                   <div>
-                    <div className='flex justify-between text-lg pb-3'>
+                    <div className='flex justify-between pb-3'>
                       <p className=' flex items-center gap-2'>
-                        <span><HiOutlineClock/></span>
-                        <span>Duration</span>
+                        <span className='text-lg'><HiOutlineClock/></span>
+                        <span className='max-xl:text-lg max-md:text-base'>Duration</span>
                       </p>
                       <p>17 Weeks</p>
                     </div>
                     <hr className='custom-hr' />
 
-                    <div className='flex justify-between text-lg py-3'>
+                    <div className='flex justify-between py-3'>
                       <p className=' flex items-center gap-2'>
-                        <span><BsPeople/></span>
-                        <span>Students</span>
+                        <span className='text-lg'><BsPeople/></span>
+                        <span className='max-xl:text-lg max-md:text-base'>Students</span>
                       </p>
                       <p>189</p>
                     </div>
                     <hr className='custom-hr' />
 
-                    <div className='flex justify-between text-lg py-3'>
+                    <div className='flex justify-between py-3'>
                       <p className=' flex items-center gap-2'>
-                        <span><RiDraftLine/></span>
-                        <span>Lessons</span>
+                        <span className='text-lg'><RiDraftLine/></span>
+                        <span className='max-xl:text-lg max-md:text-base'>Lessons</span>
                       </p>
                       <p>18</p>
                     </div>
                     <hr className='custom-hr' />
 
-                    <div className='flex justify-between text-lg py-3'>
+                    <div className='flex justify-between py-3'>
                       <p className=' flex items-center gap-2'>
-                        <span><BsBarChart/></span>
-                        <span>Skill level</span>
+                        <span className='text-lg'><BsBarChart/></span>
+                        <span className='max-xl:text-lg max-md:text-base'>Skill level</span>
                       </p>
                       <p>Expert</p>
                     </div>
                     <hr className='custom-hr' />
 
-                    <div className='flex justify-between text-lg py-3'>
+                    <div className='flex justify-between py-3'>
                       <p className=' flex items-center gap-2'>
-                        <span><RiArtboardLine/></span>
-                        <span>Quizzes</span>
+                        <span className='text-lg'><RiArtboardLine/></span>
+                        <span className='max-xl:text-lg max-md:text-base'>Quizzes</span>
                       </p>
                       <p>0</p>
                     </div>
                     <hr className='custom-hr' />
 
-                    <div className='flex justify-between text-lg py-3'>
+                    <div className='flex justify-between py-3'>
                       <p className=' flex items-center gap-2'>
-                        <span><RiAwardLine/></span>
-                        <span>Certifications</span>
+                        <span className='text-lg'><RiAwardLine/></span>
+                        <span className='max-xl:text-lg max-md:text-base'>Certifications</span>
                       </p>
                       <p>Yes</p>
                     </div>
                     <hr className='custom-hr' />
 
-                    <div className='flex justify-between text-lg py-3'>
+                    <div className='flex justify-between py-3'>
                       <p className=' flex items-center gap-2'>
-                        <span><BsPercent/></span>
-                        <span>Pass Percentage</span>
+                        <span className='text-lg'><BsPercent/></span>
+                        <span className='max-xl:text-lg max-md:text-base'>Pass Percentage</span>
                       </p>
                       <p>80%</p>
                     </div>
                     <hr className='custom-hr' />
 
-                    <div className='flex justify-between text-lg py-3'>
+                    <div className='flex justify-between py-3'>
                       <p className=' flex items-center gap-2'>
-                        <span><AiTwotoneCalendar/></span>
-                        <span>Deadline</span>
+                        <span className='text-lg'><AiTwotoneCalendar/></span>
+                        <span className='max-xl:text-lg max-md:text-base'>Deadline</span>
                       </p>
                       <p>25 Dec, 2023</p>
                     </div>
                     <hr className='custom-hr' />
 
-                    <div className='flex justify-between text-lg py-3'>
+                    <div className='flex justify-between py-3'>
                       <p className=' flex items-center gap-2'>
-                        <span><MdPersonOutline/></span>
-                        <span>Instructor</span>
+                        <span className='text-lg'><MdPersonOutline/></span>
+                        <span className='max-xl:text-lg max-md:text-base'>Instructor</span>
                       </p>
                       <p>James Carlson</p>
                     </div>
                   </div>
 
                   {/* price  */}
-                  <div className=' text-center py-3 mt-3 text-xl btn'>
+                  <div className=' text-center py-3 mt-5 text-lg btn'>
                     <p className=' select-none'>Price $49.00</p>
                   </div>
                 </div>
@@ -125,73 +125,74 @@ const Detail = () => {
             {/* detail left end  */}
 
             {/* detail right start */}
-            <div className=" w-[50%] flex flex-col gap-5 p-5">
+            <div className="w-[55%] max-xl:w-full flex flex-col gap-5 py-10 pr-10 max-xl:pl-10 max-[500px]:px-5">
               <img
                 src="https://eduvibe.devsvibe.com/main/wp-content/uploads/2023/03/course-26.webp"
-                className="w-full h-[270px]"
+                  className="w-full h-[330px] max-xl:h-[400px] max-md:h-[330px] max-[600px]:h-[300px] max-[500px]:h-[240px] rounded-lg"
               />
-              <div className="flex justify-between">
-                <div className="flex gap-2 items-center">
-                  <img
-                    src="https://eduvibe.devsvibe.com/main/wp-content/uploads/2022/11/instructor-03-02-2-100x100.webp"
-                    className=" w-8 rounded-full"
-                  />
-                  <p className="tableTitle text-sm tracking-wide">
-                    Janet Fleming
-                  </p>
-                </div>
-                <div className="tableTitle flex items-center gap-2">
-                  <RxCalendar className=" text-lg" />
-                  <p className="text-sm tracking-wide">09 Feb, 2023</p>
-                </div>
-                <div className="tableTitle flex items-center gap-2">
-                  <TiMessages className=" text-lg" />
-                  <p className="text-sm tracking-wide">0 Comments</p>
-                </div>
-                <div className="tableTitle flex items-center gap-1">
-                  <RiEyeLine className=" text-lg" />
-                  <p className="text-sm tracking-wide">1 Min Read</p>
-                </div>
+              <div className="flex justify-between max-[600px]:flex-col max-[600px]:gap-3">
+                  <div className="flex gap-2 items-center">
+                    <img
+                      src="https://eduvibe.devsvibe.com/main/wp-content/uploads/2022/11/instructor-03-02-2-100x100.webp"
+                      className=" w-8 rounded-full"
+                    />
+                    <p className="tableTitle text-sm tracking-wide">
+                      Janet Fleming
+                    </p>
+                  </div>
+                  <div className="tableTitle flex items-center gap-2">
+                    <RxCalendar className=" text-lg" />
+                    <p className="text-sm tracking-wide">09 Feb, 2023</p>
+                  </div>
+                  <div className="tableTitle flex items-center gap-2">
+                    <TiMessages className=" text-lg" />
+                    <p className="text-sm tracking-wide">0 Comments</p>
+                  </div>
+                  <div className="tableTitle flex items-center gap-1">
+                    <RiEyeLine className=" text-lg" />
+                    <p className="text-sm tracking-wide">1 Min Read</p>
+                  </div>
               </div>
               <div className=" flex flex-col gap-4">
-                <h1 className="text-gray-50 text-2xl font-medium">
-                  Social Media Marketing MASTERY( A-Z ) Journey
-                </h1>
-                <p className="tableTitle text-sm tracking-wide">
-                  Grursus mal suada faci lisis Lorem ipsum dolarorit more
-                  ametion consectetur elit. Vesti at bulum nec the odio aea the
-                  dumm ipsumm ipsum that dolocons rsus mal suada and fadolorit
-                  to the consectetur elit. All the Lorem Ipsum generators on the
-                  Internet tend to repeat that predefined chunks as necessary
-                  making this the first true dummy generator on the
-                  Internet.Grursus mal suada faci lisis Lorem ipsum dolarorit
-                  more ametion consectetur elit. Vesti at bulum nec odio aea the
-                  dumm ipsumm ipsum that dolocons rsus mal suada and fadolorit
-                  to the consectetur elit. Grursus mal suada faci lisis Lorem
-                  ipsum dolarorit more ametion consectetur elit. Vesti at bulum
-                  nec at odio aea the dumm ipsumm ipsum that dolocons rsus mal
-                  suada and fadolorit to the consectetur elit. All the Lorem
-                  Ipsum generators on the Internet tend.
-                </p>
-                <div className=" bg-color shadow-lg p-5 rounded">
-                  <p className=" text-gray-50 text-xl tracking-wide text-center">
-                    “ Duis vel scelerisque augue, ut vehicula nisl. Curabitur et
-                    mollis tortor, eget sollicitudin lectus. Praesent a ante ac
-                    urna ultrices volutpat at sed ligula. Phasellus nibh dui,
-                    pulvinar a nibh quis. ”
+                  <h1 className="text-gray-50 text-xl font-medium">
+                    Social Media Marketing MASTERY( A-Z ) Journey
+                  </h1>
+                  <p className="tableTitle text-sm tracking-wide">
+                    Grursus mal suada faci lisis Lorem ipsum dolarorit more
+                    ametion consectetur elit. Vesti at bulum nec the odio aea the
+                    dumm ipsumm ipsum that dolocons rsus mal suada and fadolorit
+                    to the consectetur elit. All the Lorem Ipsum generators on the
+                    Internet tend to repeat that predefined chunks as necessary
+                    making this the first true dummy generator on the
+                    Internet.Grursus mal suada faci lisis Lorem ipsum dolarorit
+                    more ametion consectetur elit. Vesti at bulum nec odio aea the
+                    dumm ipsumm ipsum that dolocons rsus mal suada and fadolorit
+                    to the consectetur elit. Grursus mal suada faci lisis Lorem
+                    ipsum dolarorit more ametion consectetur elit. Vesti at bulum
+                    nec at odio aea the dumm ipsumm ipsum that dolocons rsus mal
+                    suada and fadolorit to the consectetur elit. All the Lorem
+                    Ipsum generators on the Internet tend.
                   </p>
-                </div>
-                <p className="tableTitle text-sm tracking-wide">
-                  Grursus mal suada faci lisis Lorem ipsum dolarorit more
-                  ametion consectetur elit. Vesti at bulum nec the odio aea the
-                  dumm ipsumm ipsum that dolocons rsus mal suada and fadolorit
-                  to the consectetur elit. All the Lorem Ipsum generators on the
-                  Internet tend to repeat that predefined chunks as necessary
-                  making this the first true dummy generator on the
-                  Internet.Grursus mal suada faci lisis Lorem ipsum dolarorit
-                  more ametion consectetur elit.
-                </p>
+                  <div className=" bg-color shadow-lg p-5 rounded">
+                    <p className=" text-gray-50 text-lg tracking-wide text-center">
+                      “ Duis vel scelerisque augue, ut vehicula nisl. Curabitur et
+                      mollis tortor, eget sollicitudin lectus. Praesent a ante ac
+                      urna ultrices volutpat at sed ligula. Phasellus nibh dui,
+                      pulvinar a nibh quis. ”
+                    </p>
+                  </div>
+                  <p className="tableTitle text-sm tracking-wide">
+                    Grursus mal suada faci lisis Lorem ipsum dolarorit more
+                    ametion consectetur elit. Vesti at bulum nec the odio aea the
+                    dumm ipsumm ipsum that dolocons rsus mal suada and fadolorit
+                    to the consectetur elit. All the Lorem Ipsum generators on the
+                    Internet tend to repeat that predefined chunks as necessary
+                    making this the first true dummy generator on the
+                    Internet.Grursus mal suada faci lisis Lorem ipsum dolarorit
+                    more ametion consectetur elit.
+                  </p>
               </div>
+              
             </div>
             {/* detail right end */}
           </div>
