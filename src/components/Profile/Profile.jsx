d81d2933fcdaf9b5d86fe6e743140d984ae97e55
PhyoWunnaLin <phyowunnalin1214@gmail.com>
2023-07-03 23:51:54 +0630

    profile update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -16,13 +16,13 @@ const Profile = () => {
   
         <div className={`mt-[52px] duration-500 ${isSidebarOpen && " ml-[230px]" } max-lg:ml-0`}>
           <div className="lg:h-auto md:h-auto h-auto w-full flex justify-center">
-              <div className="   w-[98.5%] mt-2 mb-1  flex flex-wrap justify-center">
+              <div className="   w-[98.5%] lg:mt-1 lg:mb-1 my-[22px] flex flex-wrap justify-center">
                 {/* left */}
-                <div className="w-[90%] lg:w-[35%] md:w-[80%] lg:h-screen h-[700px]  flex justify-center ">
+                <div className="w-[96%] lg:w-[35%] md:w-[80%] lg:h-screen h-[700px]  flex justify-center ">
                   <ProfileLeft />
                 </div>
                 {/* right */}
-                <div className="w-[90%] lg:w-[65%] md:w-full pt-[14px] ">
+                <div className="w-[96%] lg:w-[65%] md:w-[80%] pt-[14px] ">
                   <ProfileRight />
                 </div>
               </div>
