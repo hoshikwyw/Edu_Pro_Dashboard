7028afb9926dfa7f3e86d7f4966e2bf9eda5daf8
thaepannphyu <phyu24655@gmail.com>
2023-07-04 23:04:25 +0630

    text/bar

--- src/components/TextEditor/EditorFun.jsx
+++ src/components/TextEditor/EditorFun.jsx
@@ -1,71 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import "./Editor.css";
+// import { document } from "postcss";
 
 const EditorFun = ({ initialize, setValueF }) => {
   const [text, setText] = useState("");
   const [value, setValue] = useState("<p>Text your description</p>");
-
-  const body = document.querySelector("#body");
-  const iframe = document.querySelector(".tox-edit-area__iframe");
-  const contentBody = document.querySelector(".tox-editor-header");
-  const menubar = document.querySelector(".tox-menubar");
-  const overload = document.querySelector(".tox-toolbar-overlord");
-  const toolBarPrimary = document.querySelector(".tox-toolbar__primary");
-  const statusbar = document.querySelector(".tox-statusbar");
-  const border = document.querySelector("tox-tinymce");
-  const selectorBg = document.querySelectorAll(".tox-tbtn--bespoke");
-  const labelColor = document.querySelectorAll(".tox-mbtn__select-label");
-  const inputChoiceColor = document.querySelectorAll(".tox-tbtn__select-label");
-  const wordCountFooterColor = document.querySelector(
-    ".tox-statusbar__wordcount"
-  );
-  const contentText = document.querySelector(".mce-content-body");
-
-  const htmltagAll = document.querySelectorAll(".tox-statusbar__path");
-  const borderBody = document.querySelector(".tox-tinymce");
-
-  useEffect(() => {
-    const iconSvgColor = document.querySelectorAll(".tox-tbtn svg");
-    if (iframe) iframe.style.backgroundColor = "transparent";
-    if (contentBody) contentBody.style.backgroundColor = "transparent";
-    if (menubar) menubar.style.backgroundColor = "transparent";
-    if (overload) overload.style.backgroundColor = "transparent";
-    if (toolBarPrimary) toolBarPrimary.style.backgroundColor = "transparent";
-    if (statusbar) {
-      statusbar.style.backgroundColor = "transparent";
-      statusbar.style.borderTop = "1px solid transparent";
-    }
-    if (border) border.style.border = "2px solid transparent";
-    if (selectorBg) {
-      [...selectorBg].map(
-        (selector) => (selector.style.backgroundColor = "transparent")
-      );
-      // selectorBg.style.backgroundColor = "transparent";
-    }
-    if (labelColor) {
-      // labelColor.style.color = "#ffffffd9";
-      [...labelColor].map((label) => (label.style.color = "#ffffffd9"));
-    }
-    if (inputChoiceColor) {
-      [...inputChoiceColor].map(
-        (inputchoice) => (inputchoice.style.color = "#ffffffd9")
-      );
-    }
-    if (wordCountFooterColor) wordCountFooterColor.style.color = "#ffffffd9";
-    if (contentText) {
-      contentText.style.color = "#ffffffd9";
-      console.log(contentText);
-      initialValue.style.color = "#ffffffd9";
-    }
-    if (iconSvgColor) {
-      [...iconSvgColor].map((svg) => (svg.style.fill = "#ffffffd9"));
-    }
-    if (htmltagAll) {
-      [...htmltagAll].map((htmltag) => (htmltag.style.color = "#ffffffd9"));
-    }
-    if (borderBody) borderBody.style.border = "2px solid transparent";
-  }, [body, border, iframe, selectorBg, htmltagAll, window.innerWidth]);
 
   return (
     <Editor
