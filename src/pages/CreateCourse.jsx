9c531455e73abd649d3112ce8971db30bf5ce0e4
khinezinthin <khinezinthinwk1@gmail.com>
2023-07-25 16:27:37 +0630

    sidebar light

--- src/pages/CreateCourse.jsx
+++ src/pages/CreateCourse.jsx
@@ -8,6 +8,8 @@ import { StateContextCustom } from "../components/context/StateContext";
 import EditorFun from "../components/TextEditor/EditorFun";
 
 const CreateCourse = () => {
+  let bgTexture = JSON.parse(localStorage.getItem("bgTexture"));
+
   const inputCover = useRef();
   const inputVideo = useRef();
   const [value, setValueF] = useState("");
@@ -30,11 +32,11 @@ const CreateCourse = () => {
             isSidebarOpen && " ml-[230px]"
           } max-lg:ml-0`}>
           <div className="flex justify-center ">
-            <div className=" w-[95%] max-[900px]:w-[95% max-[900px]:w-[94%] max-[600px]:w-[96%] mt-10 mb-6 bgTransparent">
+            <div className={` w-[95%] max-[900px]:w-[95% max-[900px]:w-[94%] max-[600px]:w-[96%] mt-10 mb-6 ${bgTexture?.white ? "bgTransparent2 shadow2" : "bgTransparent shadow"}`}>
               <form
                 onSubmit={onSubmitHandler}
                 className=" mx-10 max-sm:mx-5 mb-8">
-                <h1 className=" text-4xl my-10 max-[900px]:my-5 text-white text-shadow max-[435px]:text-3xl max-[380px]:text-2xl createFamily tracking-wide">
+                <h1 className={`text-4xl my-10 max-[900px]:my-5 ${bgTexture?.white ? "text-black" : "text-white"} text-shadow max-[435px]:text-3xl max-[380px]:text-2xl createFamily tracking-wide`}>
                   Create New Course
                 </h1>
                 <div className=" flex flex-col">
@@ -45,8 +47,8 @@ const CreateCourse = () => {
                           inputCover.current.click();
                           console.log("click");
                         }}
-                        className=" custom-input cursor-pointer w-full h-[370px] max-xl:h-[350px] rounded-md flex justify-center items-center">
-                        <p className="text-[#bebebe] text-base">Upload cover</p>
+                        className={` ${bgTexture?.white ? "custom-input2" : "custom-input"} cursor-pointer w-full h-[370px] max-xl:h-[350px] rounded-md flex justify-center items-center`}>
+                        <p className={`${bgTexture?.white ? " text-black" : " text-[#bebebe]"} text-base`}>Upload cover</p>
                         <input
                           id="cover"
                           className=" w-full h-[370px] "
@@ -61,7 +63,7 @@ const CreateCourse = () => {
                       <div className="flex flex-col gap-8 max-[900px]:gap-5">
                         <div className=" flex flex-col gap-8 max-[900px]:gap-5">
                           <input
-                            className="   py-3 pl-4 custom-input rounded-md"
+                            className={` py-3 pl-4 ${bgTexture?.white ? "custom-input2" : "custom-input"} rounded-md`}
                             type="text"
                             placeholder="Title"
                           />
@@ -78,36 +80,36 @@ const CreateCourse = () => {
                           </div> */}
                             
                           <input
-                          className="  custom-input py-3 pl-4 rounded-md"
+                          className={`${bgTexture?.white ? "custom-input2" : "custom-input"} py-3 pl-4 rounded-md`}
                           type="text"
                           placeholder="Instructor"
                         />
 
                           <div className=" flex gap-5 w-[100%] max-[700px]:flex-col">
                             <input
-                              className="  custom-input w-full py-3 pl-4 rounded-md"
+                              className={`${bgTexture?.white ? "custom-input2" : "custom-input"} w-full py-3 pl-4 rounded-md`}
                               type="number"
                               placeholder="Duration"
                             />
                             <input
-                              className="  custom-input w-full py-3 pl-4 rounded-md"
+                              className={`${bgTexture?.white ? "custom-input2" : "custom-input"} w-full py-3 pl-4 rounded-md`}
                               type="number"
                               placeholder="Lessons"
                             />
                             <input
-                              className="  custom-input w-full py-3 pl-4 rounded-md"
+                              className={`${bgTexture?.white ? "custom-input2" : "custom-input"} w-full py-3 pl-4 rounded-md`}
                               type="text"
                               placeholder="Language"
                             />
                             <input
-                              className="  custom-input w-full py-3 pl-4 rounded-md"
+                              className={`${bgTexture?.white ? "custom-input2" : "custom-input"} w-full py-3 pl-4 rounded-md`}
                               type="number"
                               placeholder="Price"
                             />
                           </div>
 
                           <input
-                            className="   py-3 pl-4 custom-input rounded-md"
+                            className={`py-3 pl-4 ${bgTexture?.white ? "custom-input2" : "custom-input"} rounded-md`}
                             type="text"
                             placeholder="Skill level"
                           />
@@ -115,15 +117,15 @@ const CreateCourse = () => {
                        
                         <div
                               onClick={() => inputVideo.current.click()}
-                              className=" custom-input flex justify-between cursor-pointer w-full py-5 max-xl:py-3 max-[900px]:py-5 pl-4 mr-1  border-[#e5eaef] rounded-md">
-                              <p className="text-[#bebebe]">Upload video</p>
+                              className={`${bgTexture?.white ? "custom-input2" : "custom-input"} flex justify-between cursor-pointer w-full py-5 max-xl:py-3 max-[900px]:py-5 pl-4 mr-1  border-[#e5eaef] rounded-md`}>
+                              <p className={`${bgTexture?.white ? " text-black" : " text-[#bebebe]"}]`}>Upload video</p>
                               <input
-                                className=" py-3 pl-4 custom-input rounded-md"
+                                className={` py-3 pl-4 ${bgTexture?.white ? "custom-input2" : "custom-input"} rounded-md`}
                                 type="file"
                                 ref={inputVideo}
                                 hidden
                               />
-                              <p className=" text-xl pr-4 text-[#bebebe]">
+                              <p className={`text-xl pr-4 ${bgTexture?.white ? " text-black" : " text-[#bebebe]"}`}>
                                 <BiMovie />
                               </p>
                         </div>
@@ -146,7 +148,7 @@ const CreateCourse = () => {
                     </p>
                   </div> */}
 
-                  <div className="custom-input pt-4 pb-2 pl-4 rounded-md flex justify-center my-8 max-[900px]:my-5">
+                  <div className=" custom-input pt-4 pb-2 pl-4 rounded-md flex justify-center my-8 max-[900px]:my-5">
                       <div className="w-[98%]">
                           <EditorFun setValueF={setValueF} />
                       </div>
@@ -156,7 +158,7 @@ const CreateCourse = () => {
                 {/* <Link to="/"> */}
                 <button
                   id="submit"
-                  className=" glass-btn py-4 px-20 max-[400px]:py-3 max-[400px]:px-14 text-lg rounded-md text-white">
+                  className={`${bgTexture?.white ? "bg-blue-700 hover:bg-blue-950 duration-500" : "glass-btn"}  py-4 px-20 max-[400px]:py-3 max-[400px]:px-14 text-lg rounded-md text-white`}>
                   <span></span>
                   <span></span>
                   <span></span>
